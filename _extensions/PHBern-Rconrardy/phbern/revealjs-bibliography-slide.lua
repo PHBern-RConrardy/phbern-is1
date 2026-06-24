b7bd24676c23d6299dcf11e3786e7c1c40788502
Richard Conrardy <richard.conrardy@phbern.ch>
2026-06-24 23:56:33 +0200

    Update project

--- _extensions/PHBern-Rconrardy/phbern/revealjs-bibliography-slide.lua
+++ _extensions/PHBern-Rconrardy/phbern/revealjs-bibliography-slide.lua
@@ -1,4 +1,31 @@
+local function add_markdown_blocks(doc, markdown)
+  local blocks = pandoc.read(markdown, "markdown").blocks
+
+  for _, block in ipairs(blocks) do
+    table.insert(doc.blocks, block)
+  end
+end
+
 function Pandoc(doc)
+  add_markdown_blocks(doc, [[
+## Rechtliches
+
+:::: {.columns}
+
+::: {.column width="50%"}
+![](_extensions/PHBern-Rconrardy/phbern/svgrepo_soco-st-various-illustration-vectors/492964_male-judge-upper-body.svg)
+:::
+
+::: {.column width="50%"}
+### Urheberrecht
+
+- Bilder wie das links: CC BY [Soco St](https://www.svgrepo.com/author/soco-st/) via SVG Repo
+- Eigene Slides unter [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/deed.en)
+:::
+
+::::
+]])
+
   table.insert(
     doc.blocks,
     pandoc.Header(2, "Literatur", pandoc.Attr("", { "smaller", "scrollable" }, {}))
